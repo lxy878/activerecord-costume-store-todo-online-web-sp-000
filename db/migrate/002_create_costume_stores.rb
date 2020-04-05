d3affdc04bc9847876f58f3a57f0301732418c3e
Xiaoyi Liu <lxy878@gmail.com>
2020-04-05 16:18:10 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -2,7 +2,12 @@
 class CreateCostumeStores < ActiveRecord::Migration[5.2]
   def change
     create_table :CostumeStores do |t|
-      
+      :name
+      :location
+      :number_of_costumes
+      :number_of_employees
+      :in_business
+      :opening_time
     end
   end
 end
