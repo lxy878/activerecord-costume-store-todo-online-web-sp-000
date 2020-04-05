2473618dcd9fcadc46bc909ae953145be93c7ab5
Xiaoyi Liu <lxy878@gmail.com>
2020-04-05 16:33:15 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -12,34 +12,4 @@
 
 ActiveRecord::Schema.define(version: 3) do
 
-  create_table "CostumeStores", force: :cascade do |t|
-    t.string "name"
-    t.string "location"
-    t.integer "number_of_costumes"
-    t.integer "number_of_employees"
-    t.boolean "in_business"
-    t.datetime "opening_time"
-    t.datetime "closing_time"
-  end
-
-  create_table "Costumes", force: :cascade do |t|
-    t.string "name"
-    t.decimal "price"
-    t.integer "size"
-    t.string "image_url"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "HauntedHouses", force: :cascade do |t|
-    t.string "name"
-    t.string "location"
-    t.string "theme"
-    t.decimal "price"
-    t.boolean "family_friendly"
-    t.datetime "opening_date"
-    t.datetime "closing_date"
-    t.text "description"
-  end
-
 end
