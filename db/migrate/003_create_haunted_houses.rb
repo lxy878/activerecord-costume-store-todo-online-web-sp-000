209cbf40f4358b0d59665f135c6cf0417492665c
Xiaoyi Liu <lxy878@gmail.com>
2020-04-05 16:22:55 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,6 +1,15 @@
 # Create your haunted_houses migration here
 class CreateHauntedHouses < ActiveRecord::Migration[5.2]
   def change
-    create_table :
+    create_table :HauntedHouses do |t|
+      t.string :name
+      t.string :location
+      t.string :theme
+      t.string :price
+      t.string :family_friendly
+      t.string :opening_date
+      t.string :closing_date
+      t.string :description
+    end
   end
 end
